b16cd570096233f30cffa3cf903f258c78560e77
Tim Mikeladze <tim.mikeladze@gmail.com>
2023-12-31 00:22:31 -0800

    feat: wip

--- examples/next-realtime-example/src/app/layout.tsx
+++ examples/next-realtime-example/src/app/layout.tsx
@@ -4,6 +4,11 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
+import { NextRealtimeStreamProvider } from 'next-realtime/react';
+import { createRealtimeSessionId } from 'next-realtime/server';
+
+import { revalidateTag } from 'next/cache';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -13,8 +18,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <NextRealtimeStreamProvider
+      revalidateTag={async (tag: string) => {
+        'use server';
+
+        revalidateTag(tag);
+      }}
+      sessionId={async () => {
+        'use server';
+
+        return createRealtimeSessionId();
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </NextRealtimeStreamProvider>
   );
 }
--- examples/next-realtime-example/src/app/page.tsx
+++ examples/next-realtime-example/src/app/page.tsx
@@ -1,26 +1,7 @@
-import { NextRealtimeStreamProvider } from 'next-realtime/react';
-import { createRealtimeSessionId } from 'next-realtime/server';
-
-import { revalidateTag } from 'next/cache';
 import TodoList from '../components/TodoList';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  return (
-    <NextRealtimeStreamProvider
-      revalidateTag={async (tag: string) => {
-        'use server';
-
-        revalidateTag(tag);
-      }}
-      sessionId={async () => {
-        'use server';
-
-        return createRealtimeSessionId();
-      }}
-    >
-      <TodoList />
-    </NextRealtimeStreamProvider>
-  );
+  return <TodoList />;
 }
